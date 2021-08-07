561f87613818e562cdbc8132468f25d457ad72d4
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-08-07 15:05:08 -0400

    commit tree 2ae89f7aff14540f1747dfb76a97ca39ad944abf

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let v = document.getElementById("text");
+        v.innerHTML = "This is really cool!"
+})
+
+
+// function content(text){
+//    let v = document.getElementById("text");
+//    v.innerHTML = text;
+// }
